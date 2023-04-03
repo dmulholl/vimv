82b7398e184895d1bdcc12f5278f96a447eaa707
Darren Mulholland <dmulholl@tcd.ie>
2023-04-04 00:42:42 +0100

    V2 refactor

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,11 @@ use std::process::exit;
 use std::env;
 use std::collections::HashSet;
 use rand::Rng;
-use std::process::Command;
 use std::io::Read;
 use colored::*;
 
 
-const HELP: &str = "
+const HELPTEXT: &str = "
 Usage: vimv [files]
 
   This utility lets you batch rename files using a text editor. Files to be
@@ -30,24 +29,17 @@ Usage: vimv [files]
   to overwrite a directory the program will exit with an error message and a
   non-zero status code.)
 
-  You can delete a file or directory by 'renaming' it to a blank line, but only
-  if the --delete flag has been specified. Deleted files are moved to the
-  system's trash/recycle bin.
-
-  If the --git flag is specified then git will be used to rename or delete
-  any files which are being tracked by git. (Note that this bypasses the
-  recycle bin for deleted files.)
+  You can delete a file or directory by prefixing its name with a `#` symbol.
+  Deleted files are moved to the system's trash/recycle bin.
 
 Arguments:
   [files]                   List of files to rename.
 
 Options:
-  -e, --editor <name>       Specify the editor to use.
+  -e, --editor <name>       Specify the editor to use. Overrides $EDITOR.
 
 Flags:
-  -d, --delete              Enable file deletion.
   -f, --force               Overwrite existing files.
-  -g, --git                 Use git for git-tracked files.
   -h, --help                Print this help text and exit.
   -q, --quiet               Only report errors.
   -s, --stdin               Read the list of input files from standard input.
@@ -57,11 +49,9 @@ Flags:
 
 fn main() {
     let mut parser = ArgParser::new()
-        .helptext(HELP)
+        .helptext(HELPTEXT)
         .version(env!("CARGO_PKG_VERSION"))
         .flag("force f")
-        .flag("delete d")
-        .flag("git g")
         .flag("quiet q")
         .flag("stdin s")
         .option("editor e", "");
@@ -77,24 +67,31 @@ fn main() {
     }
 
     // Assemble the list of input filenames.
-    let mut input_files: Vec<String> = parser.args.iter().map(|s| s.to_string()).collect();
+    let mut input_files: Vec<String> = parser.args.clone();
 
-    // If the --stdin flag has been set and no filenames have been specified on the command line,
-    // try reading the input filenames from standard input.
-    if input_files.len() == 0 && parser.found("stdin") {
+    // If the --stdin flag has been set, try reading from standard input.
+    if parser.found("stdin") {
         let mut buffer = String::new();
         if let Err(err) = std::io::stdin().read_to_string(&mut buffer) {
-            eprintln!("Error: failed to read from standard input.");
-            eprintln!("The OS reports: {}", err);
+            eprintln!("Error: failed to read filenames from standard input: {}", err);
             exit(1);
-        } else if !buffer.trim().is_empty() {
+        }
+        if !buffer.trim().is_empty() {
             input_files.extend(buffer.lines().map(|s| s.to_string()));
         }
     }
 
     // Bail if we have no input filenames to process.
-    if input_files.len() == 0 {
+    if input_files.is_empty() {
         exit(0);
+    }
+
+    // Sanity check - verify that no input filename begins with '#'.
+    for input_file in &input_files {
+        if input_file.starts_with("#") {
+            eprintln!("Error: input filenames cannot begin with '#'.");
+            exit(1);
+        }
     }
 
     // Sanity check - verify that all the input files exist.
@@ -115,18 +112,18 @@ fn main() {
         input_set.insert(input_file);
     }
 
-    // Fetch the string of output filenames from the editor.
-    let editor_input = input_files.join("\n");
+    // Fetch the output filenames from the editor.
+    let editor_input = input_files.join("\n") + "\n";
     let editor_output = match edit::edit(editor_input) {
-        Ok(edited) => edited,
+        Ok(edited) => edited.trim().to_string(),
         Err(err) => {
             eprintln!("Error: {}", err);
             exit(1);
         }
     };
+    let output_files: Vec<String> = editor_output.lines().map(|s| s.to_string()).collect();
 
     // Sanity check - verify that we have equal numbers of input and output filenames.
-    let output_files: Vec<String> = editor_output.lines().map(|s| s.to_string()).collect();
     if output_files.len() != input_files.len() {
         eprintln!(
             "Error: the number of input filenames ({}) does not match the number of output filenames ({}).",
@@ -136,9 +133,9 @@ fn main() {
         exit(1);
     }
 
-    // Sanity check - verify that the (non-empty) output filenames are unique.
+    // Sanity check - verify that the output filenames are unique.
     let mut case_sensitive_output_set = HashSet::new();
-    for output_file in output_files.iter().filter(|s| !s.is_empty()) {
+    for output_file in output_files.iter().filter(|s| !s.starts_with("#")) {
         if case_sensitive_output_set.contains(output_file) {
             eprintln!("Error: the filename '{}' appears in the output list multiple times.", output_file);
             exit(1);
@@ -146,9 +143,9 @@ fn main() {
         case_sensitive_output_set.insert(output_file);
     }
 
-    // Sanity check - verify that the (non-empty) output filenames are case-insensitively unique.
+    // Sanity check - verify that the output filenames are case-insensitively unique.
     let mut case_insensitive_output_set = HashSet::new();
-    for output_file in output_files.iter().filter(|s| !s.is_empty()).map(|s| s.to_lowercase()) {
+    for output_file in output_files.iter().filter(|s| !s.starts_with("#")).map(|s| s.to_lowercase()) {
         if case_insensitive_output_set.contains(&output_file) {
             eprintln!(
                 "Error: the filename '{}' appears multiple times in the output list (case \
@@ -171,47 +168,54 @@ fn main() {
     // Set of input files to be renamed. Used to check for cycles.
     let mut rename_set: HashSet<String> = HashSet::new();
 
-    // Populate the todo lists.
+    // Populate the task lists.
     for (input_file, output_file) in input_files.iter().zip(output_files.iter()) {
         if input_file == output_file {
             continue;
-        } else if Path::new(output_file).is_dir() {
-            if input_files.contains(output_file) {
-                rename_list.push((input_file.to_string(), output_file.to_string()));
-                rename_set.insert(input_file.to_string());
-            } else {
-                eprintln!("Error: cannot overwrite the existing directory '{}'.", output_file);
-                exit(1);
-            }
-        } else if output_file.is_empty() {
-            if parser.found("delete") {
-                delete_list.push(input_file);
-            } else {
-                eprintln!("Error: use the --delete flag to enable file deletion.");
-                exit(1);
-            }
-        } else if Path::new(output_file).is_file() {
-            if input_files.contains(output_file) {
-                rename_list.push((input_file.to_string(), output_file.to_string()));
-                rename_set.insert(input_file.to_string());
-            } else if parser.found("force") {
-                rename_list.push((input_file.to_string(), output_file.to_string()));
-                rename_set.insert(input_file.to_string());
-            } else {
-                eprintln!(
-                    "Error: the output file '{}' already exists, use --force to overwrite it.",
-                    output_file
-                );
-                exit(1);
-            }
-        } else {
-            rename_list.push((input_file.to_string(), output_file.to_string()));
-            rename_set.insert(input_file.to_string());
         }
+
+        if Path::new(output_file).is_dir() {
+            if input_files.contains(output_file) {
+                rename_list.push((input_file.to_string(), output_file.to_string()));
+                rename_set.insert(input_file.to_string());
+                continue;
+            }
+            eprintln!("Error: cannot overwrite the existing directory '{}'.", output_file);
+            exit(1);
+        }
+
+        if output_file.starts_with("#") {
+            delete_list.push(input_file);
+            continue;
+        }
+
+        if Path::new(output_file).is_file() {
+            if input_files.contains(output_file) {
+                rename_list.push((input_file.to_string(), output_file.to_string()));
+                rename_set.insert(input_file.to_string());
+                continue;
+            }
+
+           if parser.found("force") {
+                rename_list.push((input_file.to_string(), output_file.to_string()));
+                rename_set.insert(input_file.to_string());
+                continue;
+            }
+
+            eprintln!(
+                "Error: the output file '{}' already exists, use --force to overwrite it.",
+                output_file
+            );
+            exit(1);
+        }
+
+        rename_list.push((input_file.to_string(), output_file.to_string()));
+        rename_set.insert(input_file.to_string());
     }
 
-    // Check for cycles. If we find src being renamed to dst where dst is an input file that hasn't
-    // yet been deleted or renamed, we rename src to tmp instead and later rename tmp to dst.
+    // Check for cycles. If we find [src] being renamed to [dst] where [dst] is an input file that
+    // hasn't yet been deleted or renamed, we rename [src] to [tmp] instead and later rename [tmp]
+    // to [dst].
     for i in 0..rename_list.len() {
         if rename_set.contains(&rename_list[i].1) {
             let temp_file = get_temp_filename(&rename_list[i].0);
@@ -223,12 +227,12 @@ fn main() {
 
     // Deletion loop. We haven't made any changes to the file system up to this point.
     for input_file in delete_list {
-        delete_file(input_file, parser.found("git"), parser.found("quiet"));
+        delete_file(input_file, parser.found("quiet"));
     }
 
     // Rename loop.
     for (input_file, output_file) in rename_list {
-        move_file(&input_file, &output_file, parser.found("git"), parser.found("quiet"));
+        move_file(&input_file, &output_file, parser.found("quiet"));
     }
 }
 
@@ -242,41 +246,28 @@ fn get_temp_filename(base: &str) -> String {
             return candidate;
         }
     }
-    eprintln!("Error: cannot generate a temporary filename of the form '{}.vimv_temp_XXXX'.", base);
+    eprintln!(
+        "Error: failed to generate a unique temporary filename of the form '{}.vimv_temp_XXXX'.",
+        base
+    );
     exit(1);
 }
 
 
-// Delete the specified file using 'git rm' or move it to the system's trash/recycle bin.
-fn delete_file(input_file: &str, use_git: bool, quiet: bool) {
+// Move the specified file to the system's trash/recycle bin.
+fn delete_file(input_file: &str, quiet: bool) {
     if !quiet {
         println!("{} {}", "Deleting".green().bold(), input_file);
     }
-    if use_git && is_git_tracked(input_file) {
-        match Command::new("git").arg("rm").arg("-r").arg(input_file).output() {
-            Err(err) => {
-                eprintln!("Error: cannot 'git rm' the file '{}'.", input_file);
-                eprintln!("The OS reports: {}", err);
-                exit(1);
-            },
-            Ok(output) => {
-                if !output.status.success() {
-                    eprintln!("Error: cannot 'git rm' the file '{}'.", input_file);
-                    eprintln!("Git reports: {}", String::from_utf8_lossy(&output.stderr).trim());
-                    exit(1);
-                }
-            }
-        };
-    } else if let Err(err) = trash::delete(input_file) {
-        eprintln!("Error: cannot delete the file '{}'.", input_file);
-        eprintln!("The OS reports: {}", err);
+    if let Err(err) = trash::delete(input_file) {
+        eprintln!("Error: cannot delete the file '{}': {}", input_file, err);
         exit(1);
     }
 }
 
 
 // Rename `input_file` to `output_file`.
-fn move_file(input_file: &str, output_file: &str, use_git: bool, quiet: bool) {
+fn move_file(input_file: &str, output_file: &str, quiet: bool) {
     if !quiet {
         println!("{} {}", "Renaming".green().bold(), input_file);
         println!("      {}  {}", "⮑".green().bold(), output_file);
@@ -284,45 +275,13 @@ fn move_file(input_file: &str, output_file: &str, use_git: bool, quiet: bool) {
     if let Some(parent_path) = Path::new(output_file).parent() {
         if !parent_path.is_dir() {
             if let Err(err) = std::fs::create_dir_all(parent_path) {
-                eprintln!("Error: cannot create the required directory '{}'.", parent_path.display());
-                eprintln!("The OS reports: {}", err);
+                eprintln!("Error: cannot create the required directory '{}': {}", parent_path.display(), err);
                 exit(1);
             }
         }
     }
-    if use_git && is_git_tracked(input_file) {
-        match Command::new("git").arg("mv").arg("-f").arg(input_file).arg(output_file).output() {
-            Err(err) => {
-                eprintln!("Error: cannot 'git mv' the file '{}' to '{}'.", input_file, output_file);
-                eprintln!("The OS reports: {}", err);
-                exit(1);
-            },
-            Ok(output) => {
-                if !output.status.success() {
-                    eprintln!("Error: cannot 'git mv' the file '{}' to '{}'.", input_file, output_file);
-                    eprintln!("Git reports: {}", String::from_utf8_lossy(&output.stderr).trim());
-                    exit(1);
-                }
-            }
-        };
-    } else if let Err(err) = std::fs::rename(input_file, output_file) {
-        eprintln!("Error: cannot rename the file '{}' to '{}'.", input_file, output_file);
-        eprintln!("The OS reports: {}", err);
+    if let Err(err) = std::fs::rename(input_file, output_file) {
+        eprintln!("Error: cannot rename the file '{}' to '{}': {}", input_file, output_file, err);
         exit(1);
-    }
-}
-
-
-// Returns true if the file is being tracked by git.
-fn is_git_tracked(file: &str) -> bool {
-    match Command::new("git").arg("ls-files").arg("--error-unmatch").arg(file).output() {
-        Err(err) => {
-            eprintln!("Error: cannot check if the file '{}' is being tracked by git.", file);
-            eprintln!("The OS reports: {}", err);
-            exit(1);
-        },
-        Ok(output) => {
-            output.status.success()
-        }
     }
 }
